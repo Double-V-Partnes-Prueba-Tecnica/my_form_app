2da949cf957ed9cee7b4a2b317af214248268c77
ivanramirez_git <ivanrene10@gmail.com>
2023-02-22 21:16:19 -0500

    Global State

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'flutter/bloc/global/global_bloc.dart';
+
 void main() {
   runApp(const MyApp());
+}
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<GlobalBloc>(
+          create: (context) => GlobalBloc(),
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
